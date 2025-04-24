2b6945f47604bd357be4e2f2edb5d54a9915d18b
kevjidji <kdji@andrew.cmu.edu>
2025-04-24 10:56:38 -0400

    Update asr_trainer.py

--- hw4lib/trainers/asr_trainer.py
+++ hw4lib/trainers/asr_trainer.py
@@ -414,7 +414,7 @@ class ASRTrainer(BaseTrainer):
 
                 # TODO: Initialize prompts as a batch of SOS tokens
                 batch_size = feats.size(0)
-                prompts = torch.full([batch_size,1],self.tokenizer.sos_id)
+                prompts = torch.full([batch_size,1],self.tokenizer.sos_id).to(self.device)
 
 
                 # TODO: Generate sequences
